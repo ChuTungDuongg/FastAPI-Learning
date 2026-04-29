90ceecbb44d9d48e4a0fcfcaa0412ce62ef29dcc
unknown <tungduong156gli@gmail.com>
2026-04-30 01:28:10 +0700

    adding test

--- TodoApp/test/test_example.py
+++ TodoApp/test/test_example.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_equal_or_not_equal():
     assert 3 == 3
     assert 3 != 1
@@ -21,3 +23,26 @@ def test_greater_and_less_than():
     assert not (5 < 3)
     assert not (2 > 4)
     
+def test_list():
+    num_list = [1, 2, 3, 4, 5]
+    any_list = [False, True]
+    assert 1 in num_list
+    assert 6 not in num_list
+    assert all(num_list)
+    
+class Student:
+    def __init__(self, first_name, last_name, major : str, years : int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.major = major
+        self.years = years
+
+@pytest.fixture
+def default_employee():
+    return Student('John', 'Doe', 'Computer Science', 2)
+      
+def test_person_initialization(default_employee):
+    assert default_employee.first_name == "John", "first name should be John"
+    assert default_employee.last_name == "Doe", "last name should be Doe"
+    assert default_employee.major == "Computer Science", "major should be Computer Science"
+    assert default_employee.years == 2, "years should be 2"
