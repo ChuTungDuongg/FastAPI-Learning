3e0810ebbf0835cd5e04d500dd25fbd420a54a0d
unknown <tungduong156gli@gmail.com>
2026-05-01 03:10:09 +0700

    adding 179

--- TodoApp/test/test_auth.py
+++ TodoApp/test/test_auth.py
@@ -1,5 +1,8 @@
 from .utils import *
-from routers.auth import get_db, authenticate_user
+from routers.auth import ALGORITHM, SECRET_KEY, get_db, authenticate_user, create_access_token
+from fastapi import status
+from jose import jwt
+from datetime import timedelta, datetime, timezone
 
 
 app.dependency_overrides[get_db] = override_get_db
@@ -16,3 +19,16 @@ def test_authenticate_user(test_user):
     
     wrong_password_user = authenticate_user(test_user.username, 'wrongpassword', db)
     assert wrong_password_user is False
+    
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'user'
+    expires_delta = timedelta(days = 1)
+    
+    token = create_access_token(username, user_id, role, expires_delta)
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM], options={"verify_signature": False})
+    
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role
